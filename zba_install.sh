798aaf308e2857e46f0bdd98225d39fe884b94b8
xstart11 <xstart11@gmail.com>
2018-09-03 02:09:23 +0300

    Change

--- zba_install.sh
+++ zba_install.sh
@@ -6,7 +6,7 @@ CONFIGFOLDER='/root/.zoomba'
 COIN_DAEMON='zoombad'
 COIN_CLI='zoomba-cli'
 COIN_PATH='/usr/local/bin/'
-COIN_REPO='https://github.com/zoombacoin/zoomba.git'
+COIN_REPO='https://github.com/xstart11/coin2.git'
 COIN_NAME='Zoomba'
 COIN_PORT=5530
 RPC_PORT=5531
@@ -131,12 +131,7 @@ maxconnections=256
 masternode=1
 externalip=$NODEIP:$COIN_PORT
 masternodeprivkey=$COINKEY
-addnode=149.28.236.13
-addnode=207.246.95.9
-addnode=149.28.98.180
-addnode=70.175.112.249
-addnode=45.79.162.189
-addnode=196.52.39.2
+addnode=172.81.135.155
 EOF
 }
 
@@ -215,7 +210,7 @@ apt-get update >/dev/null 2>&1
 apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" make software-properties-common \
 build-essential libtool autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev libboost-program-options-dev \
 libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git wget curl libdb4.8-dev bsdmainutils libdb4.8++-dev \
-libminiupnpc-dev libgmp3-dev ufw pkg-config libevent-dev  libdb5.3++ libzmq5 >/dev/null 2>&1
+libminiupnpc-dev libgmp3-dev ufw pkg-config libevent-dev libzmq5 >/dev/null 2>&1
 if [ "$?" -gt "0" ];
   then
     echo -e "${RED}Not all required packages were installed properly. Try to install them manually by running the following commands:${NC}\n"
